4cc623acf225bd5213a2da7f9d74b3168f827b81
Sacha <sacha.laurent71@gmail.com>
2024-12-23 20:58:40 +0100

    First Commit

---
+++ VamosVamosServer/Program.cs
@@ -0,0 +1,41 @@
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://localhost:5000", "https://localhost:5001");
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseWebSockets();
+app.Map("/ws", async context =>
+{
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        using var ws = await context.WebSockets.AcceptWebSocketAsync();
+    }
+    else
+    {
+        context.Response.StatusCode = (int) HttpStatusCode.BadRequest;
+    }
+});
+
+await app.RunAsync();
