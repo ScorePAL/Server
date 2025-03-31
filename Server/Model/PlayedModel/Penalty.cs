be62058fd61bac3dfb32fa5c8ca622fd32e85377
CasahDev <sacha.laurent71@gmail.com>
2025-03-31 20:39:21 +0200

    Switch Penalty to DTO

--- Server/Model/PlayedModel/Penalty.cs
+++ Server/Model/PlayedModel/Penalty.cs
@@ -1,33 +1,12 @@
 namespace ScorePALServer.Model.PlayedModel;
 
-public class Penalty : IPenalty
+public class Penalty
 {
-    private PenaltyObtainingMethod obtainingMethod;
-    private PenaltyResult result;
-    private long penaltyTime;
-    private Played played;
+    public PenaltyObtainingMethod ObtainingMethod { get; set; }
 
-    public PenaltyObtainingMethod ObtainingMethod
-    {
-        get => obtainingMethod;
-        set => obtainingMethod = value;
-    }
+    public PenaltyResult Result { get; set; }
 
-    public PenaltyResult Result
-    {
-        get => result;
-        set => result = value;
-    }
+    public long PenaltyTime { get; set; }
 
-    public long PenaltyTime
-    {
-        get => penaltyTime;
-        set => penaltyTime = value;
-    }
-
-    public Played Played
-    {
-        get => played;
-        set => played = value ?? throw new ArgumentNullException(nameof(value));
-    }
+    public Played Played { get; set; }
 }
