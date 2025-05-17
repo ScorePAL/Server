0dd711923d087fa72f6f3cd3a8f0e2f8e0026e30
CasahDev <sacha.laurent71@gmail.com>
2025-05-17 15:26:56 +0200

    feat: Add OAuthConfig class for OAuth key configuration

---
+++ Shared/Configuration/ConnectionStrings.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Configuration;
+
+public class ConnectionStrings
+{
+    public string DB_USER { get; set; }
+    public string DB_PASSWORD { get; set; }
+    public string DB_NAME { get; set; }
+    public string DB_PORT { get; set; }
+}
+
---
+++ Shared/Configuration/OAuthConfig.cs
@@ -0,0 +1,6 @@
+﻿namespace Shared.Configuration;
+
+public class OAuthConfig
+{
+    public string Key { get; set; }
+}
