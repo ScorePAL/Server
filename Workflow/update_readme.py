fbd8af24d52eb8fad25f39ac65b9034bc77626fa
CasahDev <sacha.laurent71@gmail.com>
2025-05-17 18:08:43 +0200

    feat: Add GitHub Actions workflow to run Python script for updating README

--- Workflow/update_readme.py
+++ Workflow/update_readme.py
@@ -2,8 +2,8 @@ import requests
 import json
 
 swagger_url = "http://localhost:8080/swagger/v1/swagger.json"
-readme_path = "../README_test.md"
-start_marker = "## API routes"
+readme_path = "../README.md"
+start_marker = "## API Routes"
 
 resp = requests.get(swagger_url)
 data = resp.json()
@@ -11,9 +11,41 @@ data = resp.json()
 lines = [start_marker]
 for path, methods in data["paths"].items():
     for method, details in methods.items():
+
+        name = path.split("/")
+
+        if name[1] == "sse" or name[1] == "ws":
+            lines.append(f"### {name[1].upper()}")
+        else:
+            controller = name[2] if len(name) > 2 else ""
+            action = name[3] if len(name) > 3 else ""
+
+            lines.append(f"### {controller.capitalize()}-{action.capitalize()}")
+
         summary = details.get("summary", "")
-        lines.append(f"- `{method.upper()}` `{path}`: {summary}")
-lines.append(end_marker)
+        lines.append("```http")
+        lines.append(f"{method.upper()} {path}")
+        lines.append("```")
+
+        params = details.get("parameters", [])
+        body = details.get("requestBody", {})
+        if params or body:
+            lines.append(f"| Parameter | Type     | ")
+            lines.append(f"|-----------|----------|")
+            for param in details.get("parameters", []):
+                param_name = param.get("name", "")
+                param_type = param.get("type", "string")
+                lines.append(f"| {param_name} | {param_type} |")
+
+        if body:
+            content = body.get("content", {})
+            for content_type, schema in content.items():
+                if "application/json" in content_type:
+                    params = schema.get("schema", {})
+                    for param_name, param_details in params.items():
+                        param_type = param_details.split("/")[-1]
+                        lines.append(f"| Body | {param_type} |")
+
 
 with open(readme_path, "r") as f:
     content = f.read()
