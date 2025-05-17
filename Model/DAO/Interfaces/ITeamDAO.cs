9a679d13693364fc94056d995c232f87723b88f6
CasahDev <sacha.laurent71@gmail.com>
2025-05-17 15:27:42 +0200

    feat: Refactor ITeamDAO interface to simplify method signatures and improve team management

--- Model/DAO/Interfaces/ITeamDAO.cs
+++ Model/DAO/Interfaces/ITeamDAO.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using ScorePALServer.Model.TeamModel;
 using ScorePALServer.Model.UserModel;
+using ScorePALServerModel.Logic.ClubModel;
 
 namespace ScorePALServerModel.DAO.Interfaces;
 
@@ -12,31 +13,28 @@ public interface ITeamDAO
     /// <param name="page">The page to look at</param>
     /// <param name="limit">The number of team per page</param>
     /// <returns>All the teams in the page</returns>
-    public ActionResult<Team[]> GetTeams(long page, long limit);
+    public Team[] GetTeams(long page, long limit);
 
     /// <summary>
     /// Returns a team by its id
     /// </summary>
-    /// <param name="token">The user's token</param>
     /// <param name="team">The team id</param>
     /// <returns>The team</returns>
-    public ActionResult<Team> GetTeam(string token, Team team);
+    public Team GetTeam(Team team);
 
     /// <summary>
     /// Create a team with the given name and club id
     /// </summary>
-    /// <param name="token">The user's token</param>
     /// <param name="name">The name of the team</param>
-    /// <param name="clubId">The id of the club</param>
+    /// <param name="club"></param>
     /// <returns></returns>
-    public ActionResult CreateTeam(string token, string name, long clubId);
+    public Team CreateTeam(string name, Club club);
 
     /// <summary>
     /// Update a team with the given id
     /// </summary>
     /// <param name="user"></param>
-    /// <param name="id"></param>
-    /// <param name="name"></param>
+    /// <param name="team"></param>
     /// <returns></returns>
-    public ActionResult UpdateTeam(User user, long id, string name);
+    public Team UpdateTeam(User user, Team team);
 }
